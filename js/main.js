0c265f05924d62cd327f574edcded069e7a2a1c0
h3110 <desenvolvimento15@dl.com.br>
2024-03-05 14:17:12 -0300

    VS01

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.galeria').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  lazyLoad: 'ondemand', controls: false,
+   
+});
+		
+              
